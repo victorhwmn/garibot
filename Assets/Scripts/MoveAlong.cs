2abc3987c8f1be0c41990d94c113d3adf84b5ad5
Leonardo <leo.del.lama@hotmail.com>
2018-10-06 23:16:07 -0300

    Configuração background e adição de MoveAlong

---
+++ Assets/Scripts/MoveAlong.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAlong : MonoBehaviour
+{
+    public Transform[] moveAlong; //Objects that should move along the gameObject
+    void FixedUpdate ()
+    {
+        foreach (Transform t in moveAlong)
+            t.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, t.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D cc;
+    public float rSpeed = 40f; //Run Speed
+    private float hMove = 0f; //Horizontal Move
+    private bool jump = false; //Jump
+    private bool duck = false; //Duck
+
+    // Update is called once per frame
+    void Update()
+    {
+        hMove = Input.GetAxisRaw("Horizontal") * rSpeed;
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+
+        if (Input.GetButtonDown("Duck"))
+        { duck = true; }
+        else if (Input.GetButtonUp("Duck"))
+            duck = false;
+    }
+
+    void FixedUpdate()
+    {
+        cc.Move(hMove * Time.fixedDeltaTime, duck, jump);
+        jump = false;
+    }
+}
