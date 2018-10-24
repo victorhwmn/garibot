a3ab551a90e360b60216928375f4d8dbd9a8cdf0
victorhwmn <victorhwmn@gmail.com>
2018-10-24 10:55:46 -0300

    Prototipo do menu principal do jogo criado

---
+++ Assets/ButtonPlay.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("SimpleScene");
+    }
+}
