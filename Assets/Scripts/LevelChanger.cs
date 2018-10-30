b378bd26981e3ab4f0c01ad146a57c65a46cc816
n8eloy <nathaneloy@hotmail.com>
2018-10-30 05:44:40 -0300

    Finalização menu principal e de pausa

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+    private static int sceneToLoad;
+
+    // Load without fading
+    public void LoadScene (int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    // Fade before loading (needs LevelChanger prefab)
+    public void FadeToScene (int sceneIndex) {
+        sceneToLoad = sceneIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete() {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
